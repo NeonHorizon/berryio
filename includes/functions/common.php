3de9fd1219073a0daf77eccc9ae4a69b45e4647e
Daniel Bull <github@neonhorizon.co.uk>
2013-01-08 11:55:03 +0000

    Fixed bug in settings loader when checking the version Wont affect current release but needed for future releases

--- includes/functions/common.php
+++ includes/functions/common.php
@@ -157,9 +157,6 @@ function settings($file, $version = '')
 
   require_once(SETTINGS.$file.'.php');
 
-  if($version == '')
-    return;
-
-  if(!defined('GPIO_SETTINGS_VERSION') || GPIO_SETTINGS_VERSION != $version)
+  if(constant(strtoupper($file).'_SETTINGS_VERSION') != $version)
     exit(view('errors/incompatible_config', array('file' => $file)));
 }
