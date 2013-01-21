b6949f01cd2a59114bed5ad89918499089885bcd
Daniel Bull <github@neonhorizon.co.uk>
2013-01-21 19:28:46 +0000

    Fixed CLI version of board_status

--- includes/cli_views/pages/board_status.php
+++ includes/cli_views/pages/board_status.php
@@ -1,7 +1,9 @@
+<? foreach($information as $section => $section_details):?>
 
-CPU INFORMATION:
+<?=strtoupper($section)?> INFORMATION:
 
-<? foreach($information as $name => $details):?>
+<? foreach($section_details as $name => $details):?>
 <?=str_pad($name.':', 20)?> <?=$details?>
 
+<? endforeach?>
 <? endforeach?>
