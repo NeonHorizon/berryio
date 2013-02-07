d1b2d3c520fd4f50d6c3cad9478af83372d0aaa1
Daniel Bull <github@neonhorizon.co.uk>
2013-02-07 19:12:11 +0000

    Updated 0002 and 0003 manufacturer details
    Turns out TBC is not a manufacturer its To Be Confirmed. Oops :)
    Used Dons guess from here instead: http://www.raspberrypi.org/phpBB3/viewtopic.php?p=281257#p281257

--- includes/functions/board.php
+++ includes/functions/board.php
@@ -12,17 +12,17 @@ $GLOBALS['BOARD_SECTIONS'] = array(
   'Serial'   => 'Hardware',
 );
 $GLOBALS['BOARD_PI_REVISIONS'] = array(
-  '0002' => array('Model' => 'B', 'Revision' => '1.0', 'Memory' => '256MB', 'Manufacturer' => 'TBC',    'Notes' => '', ),
-  '0003' => array('Model' => 'B', 'Revision' => '1.0', 'Memory' => '256MB', 'Manufacturer' => 'TBC',    'Notes' => 'Fuses mod and D14 removed', ),
-  '0004' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Sony',   'Notes' => '', ),
-  '0005' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Qisda',  'Notes' => '', ),
-  '0006' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Egoman', 'Notes' => '', ),
-  '0007' => array('Model' => 'A', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Egoman', 'Notes' => '', ),
-  '0008' => array('Model' => 'A', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Sony',   'Notes' => '', ),
-  '0009' => array('Model' => 'A', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Qisda',  'Notes' => '', ),
-  '000d' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '512MB', 'Manufacturer' => 'Egoman', 'Notes' => '', ),
-  '000e' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '512MB', 'Manufacturer' => 'Sony',   'Notes' => '', ),
-  '000f' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '512MB', 'Manufacturer' => 'Qisda',  'Notes' => '', ),
+  '0002' => array('Model' => 'B', 'Revision' => '1.0', 'Memory' => '256MB', 'Manufacturer' => 'Egoman (?)', 'Notes' => '', ),
+  '0003' => array('Model' => 'B', 'Revision' => '1.0', 'Memory' => '256MB', 'Manufacturer' => 'Egoman (?)', 'Notes' => 'Fuses mod and D14 removed', ),
+  '0004' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Sony',       'Notes' => '', ),
+  '0005' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Qisda',      'Notes' => '', ),
+  '0006' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Egoman',     'Notes' => '', ),
+  '0007' => array('Model' => 'A', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Egoman',     'Notes' => '', ),
+  '0008' => array('Model' => 'A', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Sony',       'Notes' => '', ),
+  '0009' => array('Model' => 'A', 'Revision' => '2.0', 'Memory' => '256MB', 'Manufacturer' => 'Qisda',      'Notes' => '', ),
+  '000d' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '512MB', 'Manufacturer' => 'Egoman',     'Notes' => '', ),
+  '000e' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '512MB', 'Manufacturer' => 'Sony',       'Notes' => '', ),
+  '000f' => array('Model' => 'B', 'Revision' => '2.0', 'Memory' => '512MB', 'Manufacturer' => 'Qisda',      'Notes' => '', ),
 );
 
 
