1e97055014bb01048779ce0607670b9b3ca16482
Daniel Bull <github@neonhorizon.co.uk>
2013-03-24 16:00:34 +0000

    Fixed SPI channel name pad direction on CLI

--- includes/cli_views/pages/spi_status.php
+++ includes/cli_views/pages/spi_status.php
@@ -12,7 +12,7 @@ SPI ADC VALUES:
 
 <? foreach($spi_adc_values as $chip_select => $channels):?>
 <? foreach($channels as $channel => $value):?>
-  <?=$GLOBALS['SPI_CHIP_SELECTS'][$chip_select]?> <?=str_pad($GLOBALS['SPI_CHANNELS'][$chip_select][$channel], 10, ' ',  STR_PAD_LEFT)?> <?=str_pad($value, 4, ' ', STR_PAD_LEFT)?>  <?=graph_horizontal_bar($value, 0, 1023)?>
+  <?=$GLOBALS['SPI_CHIP_SELECTS'][$chip_select]?> <?=str_pad($GLOBALS['SPI_CHANNELS'][$chip_select][$channel], 10, ' ',  STR_PAD_RIGHT)?> <?=str_pad($value, 4, ' ', STR_PAD_LEFT)?>  <?=graph_horizontal_bar($value, 0, 1023)?>
 
 <? endforeach?>
 <? endforeach?>
@@ -22,7 +22,7 @@ SPI DAC SETTINGS:
 <? foreach($GLOBALS['SPI_CHANNELS'] as $chip_select => $channels):?>
 <? foreach($channels as $channel => $channel_name):?>
 <? if(isset($spi_dac_values[$chip_select][$channel])):?>
-  <?=$GLOBALS['SPI_CHIP_SELECTS'][$chip_select]?> <?=str_pad($channel_name, 10, ' ',  STR_PAD_LEFT)?> <?=str_pad($spi_dac_values[$chip_select][$channel], 4, ' ', STR_PAD_LEFT)?>  <?=graph_horizontal_bar($spi_dac_values[$chip_select][$channel], 0, 4095)?>
+  <?=$GLOBALS['SPI_CHIP_SELECTS'][$chip_select]?> <?=str_pad($channel_name, 10, ' ',  STR_PAD_RIGHT)?> <?=str_pad($spi_dac_values[$chip_select][$channel], 4, ' ', STR_PAD_LEFT)?>  <?=graph_horizontal_bar($spi_dac_values[$chip_select][$channel], 0, 4095)?>
 
 <? endif?>
 <? endforeach?>
