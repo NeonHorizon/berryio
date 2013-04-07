beb0bfde287364ee659cb27e88faacc2eadabce0
Daniel Bull <github@neonhorizon.co.uk>
2013-04-08 00:12:36 +0100

    Fixed indentation in Javascript

--- includes/views/html/javascript/updateSPIValues.php
+++ includes/views/html/javascript/updateSPIValues.php
@@ -17,14 +17,14 @@
         var channels = xmlhttp.responseText.split('\n');
         if(channels[0] == 'OK:')
         {
-        	for(channel in channels)
+          for(channel in channels)
           {
             if(channel != 0 && channels[channel] != '')
             {
-          	  channelInfo=channels[channel].split(',');
-          	  value = Math.round(channelInfo[2] / 10.23);
-          	  document.getElementById('spi_adc_'+channelInfo[0]+'_'+channelInfo[1]+'_bar').style.width=value+'px';
-          	  document.getElementById('spi_adc_'+channelInfo[0]+'_'+channelInfo[1]+'_value').innerHTML=value+'%';
+              channelInfo=channels[channel].split(',');
+              value = Math.round(channelInfo[2] / 10.23);
+              document.getElementById('spi_adc_'+channelInfo[0]+'_'+channelInfo[1]+'_bar').style.width=value+'px';
+              document.getElementById('spi_adc_'+channelInfo[0]+'_'+channelInfo[1]+'_value').innerHTML=value+'%';
             }
           }
         }
