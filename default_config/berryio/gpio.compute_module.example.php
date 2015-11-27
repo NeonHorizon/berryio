c18ba245d9c5de7c93d91696f07181b4d6000ba8
Daniel Bull <github@neonhorizon.co.uk>
2015-11-27 10:36:04 +0000

    Update gpio.compute_module.example.php

--- default_config/berryio/gpio.compute_module.example.php
+++ default_config/berryio/gpio.compute_module.example.php
@@ -76,7 +76,7 @@ $GLOBALS['GPIO_PINS'] += array(
 */
 
 
-// Settings for model A+, B+ and Pi 2 model B boards
+// Settings for model A+, B+, Pi 2 model B and Zero boards
 $GLOBALS['GPIO_PINS'] = array(
   2  => 'I2C SDA1',
   3  => 'I2C SCL1',
