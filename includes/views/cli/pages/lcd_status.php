e786055d84c9f9a533bdb8e15be262350055a317
Daniel Bull <github@neonhorizon.co.uk>
2016-08-28 22:37:18 +0100

    Clarification of what LCD config files does

--- includes/views/cli/pages/lcd_status.php
+++ includes/views/cli/pages/lcd_status.php
@@ -3,8 +3,9 @@ HINTS AND TIPS:
 
   Refer to the config file <?=SETTINGS?>lcd.php for
   instructions on wiring a HDD44780 or KS0066U compatible
-  LCD display. This includes options for changing which
-  GPIO pins you wish to connect the display to.
+  LCD display. This file also includes options for
+  changing the number of characters on the display and
+  which GPIO ports you wish to connect the display to.
 
   Always initialise the display before use with:
   sudo <?=$GLOBALS['EXEC']?> lcd_initialise
