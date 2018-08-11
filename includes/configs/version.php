041aff63e835f42af2bb8363e70ab49dc8643056
Daniel Bull <github@neonhorizon.co.uk>
2018-08-11 13:11:53 +0100

    Added installation detection and support for new Pi hardware revisions

--- includes/configs/version.php
+++ includes/configs/version.php
@@ -55,6 +55,7 @@ $GLOBALS['VERSION_HISTORY'] = array(
   array('1.14.0', '2016-09-03', array('Added support for 8 bit LCD\'s')),
   array('1.15.0', '2016-11-17', array('Updated/Corrected Raspberry Pi identification')),
   array('2.0.0',  '2017-11-03', array('Compatiblity changes for Stretch including migration to PHP7 (REQUIRES RASPBIAN STRETCH OR NEWER!)')),
+  array('2.1.0',  '2018-08-11', array('Added installation detection and support for new Pi hardware revisions')),
 );
 
 // Version Numbering
